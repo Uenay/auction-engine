5aaaa45182398aecac6768e9aa479f5a83bce267
uenay <uenay1@gmail.com>
2024-04-26 19:40:50 +0300

    init

---
+++ src/main/java/ru/fathutdinova/auctionengine/AuctionEngineApplication.java
@@ -0,0 +1,13 @@
+package ru.fathutdinova.auctionengine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuctionEngineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuctionEngineApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/fathutdinova/auctionengine/AuctionEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.fathutdinova.auctionengine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuctionEngineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
